bbfaf03707657e5c1a69993d9a705b833c39a3bf
tarun <tarundevops@flipkkart.com>
2026-09-08 13:14:20 +0000

    developer-2 added code to app.java

--- app.java
+++ app.java
@@ -18,4 +18,10 @@ public class Addition {
 
         sc.close();
     }
-}  
+}
+Developer-2
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
